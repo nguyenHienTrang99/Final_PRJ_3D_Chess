c707485ded5c7c240fc92a4f981b2592ca5dc6a0
nguyenHienTrang99 <115915656+nguyenHienTrang99@users.noreply.github.com>
2022-12-05 18:25:15 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 let colorList, board, siz, rotation, rvel, cursorPiece;
-let scaleSize = 1; //Do mo rong 
-let camspeed = 0.2; // Toc do quay 
+let scaleSize = 1; 
+let camspeed = 0.2; 
 let a = 8; 
 let b = 8; // 8 X 8
-let pieces = []; // Mảng chua cac quan co 
+let pieces = []; 
 let selected = null; 
 let turn = 'white'; 
 let mode = 'none';
@@ -13,14 +13,11 @@ let colorEven = 'red'
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
     angleMode(DEGREES);
-	// Tao mang mau 
-	colorList = [colorOdd, colorEven];
-	// Tao vecto quay . 
+    colorList = [colorOdd, colorEven];
     rotation = createVector(0, 0, 0);
     rvel = createVector(-5, 0, 0);
-    siz = floor(height /9); // Kich thuoc ca ban co 
-    // Tao mảng 2 chiều hay ban co gom cac mau xen ke
-	board = [
+    siz = floor(height /9); 
+    board = [
         [0, 1, 0, 1, 0, 1, 0, 1],
         [1, 0, 1, 0, 1, 0, 1, 0],
         [0, 1, 0, 1, 0, 1, 0, 1],
@@ -32,7 +29,6 @@ function setup() {
     ];
     let cx = (-3.5 * siz) + (5 * siz)
     let cz = (-3.5 * siz) + (4 * siz)
-	// Tạo ra một đối tượng để chỉ định các quân cờ từ lớp piece gọi là ô chỉ định 
     cursorPiece = new Piece(height / 40, createVector(cx, 0, cz), 'C', 'white')
 }
 
@@ -40,7 +36,6 @@ function preload() {
     myFont = loadFont('Reclaim.otf')
 }
 
-// Tạo hướng di chuyển trái phải lên xuống cho ô chỉ định 
 function keyPressed() {
     if (keyCode == LEFT_ARROW && cursorPiece.tpos.x > (-3 * siz)) cursorPiece.tpos.x -= siz;
     if (keyCode == RIGHT_ARROW && cursorPiece.tpos.x < (3 * siz)) cursorPiece.tpos.x += siz;
@@ -48,17 +43,13 @@ function keyPressed() {
     if (keyCode == DOWN_ARROW && cursorPiece.tpos.z < (3 * siz)) cursorPiece.tpos.z += siz;
 }
 
-// Tao backgroup cho không gian 3D
 function draw() {
     background('#FF69B4');
-	// Điều chỉnh độ sáng tối 
     ambientLight(100);
     pointLight(100, 100, 100, 0, -350, 0)
     scale(scaleSize);
-	// gọi hàm từ camera.js
     rotateEverything();
     moveCamera();
-	// Tạo từng ô cờ , trong không gian là box cờ , ta chạy 2 vòng lăp 8 X 8 để tạo ra bàn cờ trong không gian 3 chiều
     for (let row = 0; row < a; row += 1) {
         for (let col = 0; col < b; col += 1) {
             push();
@@ -72,7 +63,6 @@ function draw() {
             pop();
         }
 	}
-	// gọi phương thức cho ô chỉ định 
     cursorPiece.show();
     cursorPiece.move();
 }
